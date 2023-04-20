ecddc6c1ed30bf5d5fc28607e5333ebacd70747a
Sri Ragh Chilukamukku <sriragh@floatteam.com>
2023-04-21 02:08:12 +0530

    reverted

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
-const env = loadEnv(mode, process.cwd(), "")
-console.log("sri", env)
-  // plugins: [vue(), vueJsx()]
-  // envDir: 'src'
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // }
-
-  return {}
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+    envDir: 'src',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
-// export default defineConfig(({ command, mode, ssrBuild }) => {
-//   if (command === 'serve') {
-//     return {
-//       // dev specific config
-//     }
-//   } else {
-//     // command === 'build'
-//     return {
-//       // build specific config
-//     }
-//   }
-// })
