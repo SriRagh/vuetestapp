67bb33ade854e772b182eaab31fb1466a98a6f9e
Sri Ragh Chilukamukku <sriragh@floatteam.com>
2023-04-24 12:42:17 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-console.log(import.meta, "1", import.meta.env.__APP_ENV__, import.meta.__APP_ENV__)
+console.log(__APP_VERSION__, import.meta.env )
 import App from './App.vue'
 import router from './router'
-console.log("sri", __APP_VERSION__)
+console.log("sri", )
 import './assets/main.css'
 
 const app = createApp(App)
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,54 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+// console.log(process.env)
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueJsx()],
+//     // envDir: 'src',
+//     define: {
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-    envDir: 'src',
-    define: {
+//       __APP_VERSION__: JSON.stringify(process.env),
+      
+//    },
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-      __APP_VERSION__: JSON.stringify(process.env),
+
+export default ({ mode }) => {
+  console.log(mode)
+ 
+  const env = loadEnv("mode", process.cwd(), "");
+  const processEnvValues = {
+    "process.env": Object.entries(env).reduce((prev, [key, val]) => {
+      console.log(key, val);
+      return {
+        ...prev,
+        [key]: val,
+      };
+    }, {}),
+  };
+    return defineConfig({
+          define: {
+
+      __APP_VERSION__: processEnvValues,
       
    },
-  resolve: {
-    alias: {
+      // define: processEnvValues,
+        plugins: [vue()],
+          resolve: {
+    alias : {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+    });
+  
+}
