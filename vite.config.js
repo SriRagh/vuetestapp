92ac802283eab6b7ccceb6774d522a7a05c95a1e
Sri Ragh Chilukamukku <sriragh@floatteam.com>
2023-04-25 13:49:00 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,16 @@
-// import { fileURLToPath, URL } from 'url'
-// import dotenv from 'dotenv';
-// import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// // https://vitejs.dev/config/
-// dotenv.config({
-//   path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
-// });
-
-// export default defineConfig({
-//   plugins: [vue(), 
-//     vueJsx(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
-import { defineConfig } from 'vite';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  //   plugins: [vue(), 
-  //   vueJsx(),
-  // ],
-  build: {
-    target: 'es2020',
-    brotliSize: false,
-  },
-});
+  plugins: [vue(), 
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
